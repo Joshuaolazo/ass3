0ddc716702e3ea6afb3cbf3993167dfc94cc26e2
Joshuaolazo <joshuaolazo@gmail.com>
2018-12-04 12:53:47 -0500

    Added files

---
+++ asst3/bankingClient.c
@@ -0,0 +1,11 @@
+include "banking.h"
+
+
+int int main(int argc, char const *argv[]) {
+    if( argc != 2){
+        fprintf(stderr, "%s\n", "wrong number of input args");
+    }
+    int port = argv[1];
+    
+    return 0;
+}
