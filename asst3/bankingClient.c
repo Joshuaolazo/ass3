263f00b21128a93966f814f7ed280cb9f57fca75
Joshuaolazo <joshuaolazo@gmail.com>
2018-12-04 20:28:59 -0500

    create make file

--- asst3/bankingClient.c
+++ asst3/bankingClient.c
@@ -1,11 +1,15 @@
 include "banking.h"
 
+// Copied
+char client_message[2000];
+char buffer[1024];
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 int int main(int argc, char const *argv[]) {
     if( argc != 2){
         fprintf(stderr, "%s\n", "wrong number of input args");
     }
     int port = argv[1];
-    
+
     return 0;
 }
