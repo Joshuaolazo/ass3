3835d273259028fe07f5132e1a60d27111e135aa
dragonuity <40574785+dragonuity@users.noreply.github.com>
2018-12-04 23:05:42 -0500

    Add files via upload

---
+++ client.c
@@ -0,0 +1,143 @@
+
+// Write CPP code here 
+#include <netdb.h> 
+#include <arpa/inet.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#define MAX 256 
+#define PORT 12421 
+#define SA struct sockaddr 
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int n; 
+    for (;;) { 
+        bzero(buff, sizeof(buff)); 
+        printf("Enter the string : "); 
+        n = 0; 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+        write(sockfd, buff, sizeof(buff)); 
+        bzero(buff, sizeof(buff)); 
+        read(sockfd, buff, sizeof(buff)); 
+        printf("From Server : %s", buff); 
+        if ((strncmp(buff, "exit", 4)) == 0) { 
+            printf("Client Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
+  
+int main(int argc, char * argv[]) 
+{ 
+    int sockfd, connfd; 
+    struct sockaddr_in servaddr, cli; 
+  
+    // socket create and varification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+  	//Get IP
+  	
+ 
+ 		    struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
+    if (argc != 2) {
+        fprintf(stderr,"usage: showip hostname\n");
+        return 1;
+    }
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_STREAM;
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+    printf("IP addresses for %s:\n\n", argv[1]);
+    for(p = res;p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+        // get the pointer to the address itself,
+        // different fields in IPv4 and IPv6:
+        if (p->ai_family == AF_INET) { // IPv4
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else { // IPv6
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+    }
+    freeaddrinfo(res); // free the linked list
+ 
+ 
+ 
+ 
+ 
+ 
+ 	printf("	Dec Equiv: %d\n", inet_addr(ipstr));
+ 
+ /*
+  	//struct hostent* host;
+  	//host = getaddrinfo("cp.cs.rutgers.edu");
+  //printf("IP address = %s\n",	inet_ntop(*(long*)host->h_addr_list[0]));
+  	
+  	struct hostent *lh = gethostbyname("cp.cs.rutgers.edu");
+  	if(lh){
+  		puts(lh->h_name);
+  		output = inet_ntoa(lh->h_name);
+  		printf("%s\n",output);
+  	}
+  		else{
+  			herror("gethostbyname");
+  			return 0;
+  			}
+  	
+  	
+  	
+  	
+  	
+  	char hostname[256];
+  	hostname[256] = 0;
+  	hostname = "cp.cs.rutgers.edu";
+  	if(gethostname(hostname,256)==0)
+  		puts(hostname);
+  			else {
+  				perror("gethostname");
+  				return 0;
+  			}
+  */
+		// end
+    
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr(ipstr);
+    servaddr.sin_port = htons(PORT); 
+  
+    // connect the client socket to server socket 
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to the server..\n"); 
+  
+    // function for chat 
+    func(sockfd); 
+  
+    // close the socket 
+    close(sockfd); 
+} 
