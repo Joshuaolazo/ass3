2b78331fc02c51e585a7f6529d4ddd4df0803380
Joshuaolazo <joshuaolazo@gmail.com>
2018-12-09 22:52:39 -0500

    remove read function from andrew client
    andrew bugfix 4

--- client.c
+++ client.c
@@ -20,6 +20,7 @@ void func(int sockfd)
         while ((buff[n++] = getchar()) != '\n')
             ;
         write(sockfd, buff, sizeof(buff));
+        /*
         bzero(buff, sizeof(buff));
         read(sockfd, buff, sizeof(buff));
         printf("From Server : %s", buff);
@@ -27,6 +28,7 @@ void func(int sockfd)
             printf("Client Exit...\n");
             break;
         }
+        */
     }
 }
 
