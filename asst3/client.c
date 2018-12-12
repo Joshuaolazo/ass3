28c2bac50ca1c15d46a7cc6495db6f4a0023e5a4
Joshuaolazo <joshuaolazo@gmail.com>
2018-12-12 00:34:34 -0500

    Update client.c

--- asst3/client.c
+++ asst3/client.c
@@ -49,7 +49,7 @@ void readr(void * args)
             printf("Client Exiting\n");
             terminate = true;
 			close(sockfd);
-			pthread_cancel(in_args->readtid);
+			pthread_cancel(in_args->writetid);
 			pthread_exit(NULL);
             break;
         }
